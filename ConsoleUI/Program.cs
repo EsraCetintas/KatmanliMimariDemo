bb5e0a23d913444d9e9615062e389ef9dadfae39
esra.cetintas@outlook.com.tr <esra.cetintas@outlook.com.tr>
2021-04-15 23:44:41 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IoC
+            IProductService productService = new ProductManager(new InMemoryProductDal());
+            foreach (var product in productService.GetAll())
+            {
+                Console.WriteLine(product.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    { 
+        public List<Product> GetAll()
+        {
+            return new List<Product>
+           {
+               new Product{Id=1,Name="Laptop"},
+               new Product{Id=2,Name="Mouse"}
+           };
+        }
+    }
+}
